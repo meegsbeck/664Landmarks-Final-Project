5f959007300d56874790d8136fa2c4f38b6a2902
meegsbeck <mbeck101@pratt.edu>
2016-11-17 19:39:45 -0500

    Add files via upload
    States with locations

---
+++ nys_historic_places.py
@@ -0,0 +1,39 @@
+import csv
+import json
+
+nys_historic_places = {}
+
+with open('National_Register_of_Historic_Places.csv','r') as f:
+
+	reader = csv.reader(f)
+	
+	for row in reader:
+		
+		landmark_name = row[0]
+		county_name = row[1]
+		location = row[6]
+		
+		if county_name not in nys_historic_places:
+		
+			nys_historic_places[county_name] = {}
+		
+		if location not in nys_historic_places[county_name]:
+		
+			nys_historic_places[county_name][location] = []
+		
+		if landmark_name not in nys_historic_places[county_name][location]:
+		
+			#nys_historic_places[county_name].append(landmark_name)
+			nys_historic_places[county_name][location].append(landmark_name)
+		
+		
+		
+			
+		#if location not in nys_historic_places[county_name][landmark_name]:
+			
+		#	nys_historic_places[county_name][landmark_name].append(location)
+		
+#print(nys_historic_places)
+
+with open('scraped_state_landmarks.json', 'w') as f:
+	f.write(json.dumps(nys_historic_places,indent=4))	
